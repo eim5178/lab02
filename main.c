feff1368021f5b05e7f8b5f47ee0b750e7948ae4
eim5178 <eim5178@psu.edu>
2020-09-08 19:21:03 +0000

    \n added

--- main.c
+++ main.c
@@ -11,31 +11,31 @@
 int main(void) {
   double percent = atof(readline("Enter your CMPSC 131 grade: "));
   if (percent >= 93.0){
-    printf("Your letter grade for CMPSC 131 is A.");
+    printf("Your letter grade for CMPSC 131 is A.\n");
   }
   else if (percent >= 90.0){
-    printf("Your letter grade for CMPSC 131 is A-.");
+    printf("Your letter grade for CMPSC 131 is A-.\n");
   }
   else if (percent >= 87.0){
-    printf("Your letter grade for CMPSC 131 is B+.");
+    printf("Your letter grade for CMPSC 131 is B+.\n");
   }
   else if (percent >=83.0){
-    printf("Your letter grade for CMPSC 131 is B.");
+    printf("Your letter grade for CMPSC 131 is B.\n");
   }
   else if (percent >=80.0){
-    printf("Your letter grade for CMPSC 131 is B-.");
+    printf("Your letter grade for CMPSC 131 is B-.\n");
   }
   else if (percent >=77.0){
-    printf("Your letter grade for CMPSC 131 is C+.");
+    printf("Your letter grade for CMPSC 131 is C+.\n");
   }
   else if (percent >=70.0){
-    printf("Your letter grade for CMPSC 131 is C.");
+    printf("Your letter grade for CMPSC 131 is C.\n");
   }
   else if (percent >=60.0){
-    printf("Your letter grade for CMPSC 131 is D.");
+    printf("Your letter grade for CMPSC 131 is D.\n");
   }
   else{
-    printf("Your letter grade for CMPSC 131 is F.");
+    printf("Your letter grade for CMPSC 131 is F.\n");
   }
 
   return 0;
