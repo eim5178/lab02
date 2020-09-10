e6633b97d985a6026b01541860e0ed0260900c47
eim5178 <eim5178@psu.edu>
2020-09-10 14:06:45 +0000

    making it so that the if else p=defines a letter and it only has one print statement

--- main.c
+++ main.c
@@ -8,33 +8,34 @@
 
 int main(void) {
   double percent = atof(readline("Enter your CMPSC 131 grade: "));
+  char *letter;
   if (percent >= 93.0){
-    printf("Your letter grade for CMPSC 131 is A.\n");
+    letter = "A";
   }
   else if (percent >= 90.0){
-    printf("Your letter grade for CMPSC 131 is A-.\n");
+    letter = "A-";
   }
   else if (percent >= 87.0){
-    printf("Your letter grade for CMPSC 131 is B+.\n");
+    letter = "B+";
   }
   else if (percent >=83.0){
-    printf("Your letter grade for CMPSC 131 is B.\n");
+    letter = "B";
   }
   else if (percent >=80.0){
-    printf("Your letter grade for CMPSC 131 is B-.\n");
+    letter = "B-";
   }
   else if (percent >=77.0){
-    printf("Your letter grade for CMPSC 131 is C+.\n");
+    letter = "C+";
   }
   else if (percent >=70.0){
-    printf("Your letter grade for CMPSC 131 is C.\n");
+    letter = "C";
   }
   else if (percent >=60.0){
-    printf("Your letter grade for CMPSC 131 is D.\n");
+    letter = "D";
   }
   else{
-    printf("Your letter grade for CMPSC 131 is F.\n");
+    letter = "F";
   }
-
+printf("Your letter grade for CMPSC 131 is %s.\n" ,letter);
   return 0;
 }
